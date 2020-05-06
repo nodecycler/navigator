46884caeb4c2720969a0ced6b6e8e0f744f12168
Mathieu Maes <mathieu@zimmo.be>
2020-05-06 22:17:06 +0200

    Refactor route state to include closest routes when no current route is present

--- src/app/store/route/route.actions.ts
+++ src/app/store/route/route.actions.ts
@@ -1,5 +1,7 @@
 import {createAction, props} from '@ngrx/store';
 import {Feature, LineString, Point} from 'geojson';
+import {ClosestRoute} from './route.types';
 
 export const setCurrentRoute = createAction('[Route] Set current route', props<{route: Feature<LineString>, point: Feature<Point>}>());
 export const clearCurrentRoute = createAction('[Route] Clear');
+export const setClosestRoutes = createAction('[Route] Set closest routes', props<{routes: ClosestRoute[]}>());
--- src/app/store/route/route.types.ts
+++ src/app/store/route/route.types.ts
@@ -1,4 +1,5 @@
-import {Feature, LineString, Point} from 'geojson';
+import {Feature, FeatureCollection, LineString, Point} from 'geojson';
+import {NearestPointOnLine} from '@turf/nearest-point-on-line';
 
 export interface RouteState {
   route: Feature<LineString>;
@@ -6,4 +7,10 @@ export interface RouteState {
   bearing: number;
   routeProgress: number;
   destinationNodeId: string;
+  closestRoutes: ClosestRoute[];
+}
+
+export interface ClosestRoute {
+  route: Feature<LineString>;
+  point: NearestPointOnLine;
 }
