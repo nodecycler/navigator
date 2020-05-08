41407228806d249fef291f9bc4dc4734bdbb3015
Mathieu Maes <mathieu@zimmo.be>
2020-05-08 10:00:43 +0200

    Finish implementing route-locator

--- src/app/navigator/route-locator/route-locator.component.ts
+++ src/app/navigator/route-locator/route-locator.component.ts
@@ -1,5 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouteFacadeService} from '../../services/routeFacade.service';
+import {NodesFacadeService} from '../../services/nodesFacade.service';
+import {combineLatest, Observable} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+import {Node} from '../../store/nodes/nodes.types';
+import {GeolocationService} from '../../services/geolocation.service';
+import {bearing} from '@turf/turf';
+
+interface RouteViewInterface {
+  begin: string;
+  end: string;
+  distance: string;
+  bearingStyle: string;
+}
 
 @Component({
   selector: 'app-route-locator',
@@ -7,13 +20,44 @@ import {RouteFacadeService} from '../../services/routeFacade.service';
   styleUrls: ['./route-locator.component.scss']
 })
 export class RouteLocatorComponent implements OnInit {
-  routes;
-  constructor(private routeFacade: RouteFacadeService) { }
+  routes: RouteViewInterface[];
+
+  constructor(
+    private routeFacade: RouteFacadeService,
+    private nodesFacade: NodesFacadeService,
+    private geolocation: GeolocationService
+  ) {
+  }
 
   ngOnInit() {
-    this.routeFacade.closestRoutes$.subscribe(routes => {
+    combineLatest([this.geolocation.position$, this.routeFacade.closestRoutes$, this.nodesFacade.nodes$])
+      .pipe(
+        map(([position, routes, nodes]) => {
+          return routes.map(({route, point}) => {
+            const begin = nodes.find(node => node.id === route.properties.begin_geoid).number;
+            const end = nodes.find(node => node.id === route.properties.end_geoid).number;
+            return {
+              begin,
+              end,
+              distance: this.getDistance(point),
+              bearingStyle: `rotate(${this.getBearing(point, position.location)}deg)`,
+            };
+          });
+        })
+      ).subscribe(routes => {
       this.routes = routes;
     });
   }
 
+  getDistance(point) {
+    const distance = point.properties.dist;
+    if (distance > 1000) {
+      return `${Math.round(distance / 100) / 10} km`;
+    }
+    return `${Math.round(distance)} m`;
+  }
+
+  getBearing(point, position) {
+    return bearing([position.longitude, position.latitude], point);
+  }
 }
